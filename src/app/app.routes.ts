dc24d44c1174857dee9e67fb719cb53c0b5713ad
Sansao77 <gabrielp_santos@hotmail.com>
2024-10-18 14:58:55 -0300

    feat: add components && inicial draft

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './pages/auth/auth.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'auth', component: AuthComponent},
+    {path: 'home', component: HomeComponent},
+    {path: '', redirectTo:'/auth', pathMatch:'full'}
+];
