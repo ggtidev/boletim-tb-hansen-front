6dcd59e62cd5ec7a2aab3e81225e0211fc4aaefe
Sansao77 <gabrielp_santos@hotmail.com>
2024-10-21 23:18:16 -0300

    feat: add matTable

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,21 +1,36 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatTableModule} from '@angular/material/table'
+
+export interface mockData{
+  district: number,
+  name: string
+};
+
+const DATA:mockData[] = [
+  {
+    district: 3,
+    name: "US 106 CS PROF JOAQUIM CAVALCANTE"
+  },
+  {
+    district: 3,
+    name: "US 106 CS PROF JOAQUIM CAVALCANTE"
+  },
+  {
+    district: 3,
+    name: "US 106 CS PROF JOAQUIM CAVALCANTE"
+  },
+]
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 
 export class HomeComponent {
-  colunsTitles:string[] = ['DISTRITOS', 'UNIDADES', 'BOLETIM', 'STATUS'];
-
-  data = [
-    {
-      district: 3,
-      name: "US 106 CS PROF JOAQUIM CAVALCANTE"
-    },
-  ];
+  columnsTitles:string[] = ['distritos', 'unidades', 'boletim', 'status'];
+  data = DATA;
 }
