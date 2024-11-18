68d592c0f8a18bde08253cda790017a42a9fc248
Sansao77 <gabrielp_santos@hotmail.com>
2024-11-17 21:02:29 -0300

    feat: overlay-form design completed

--- src/app/pages/overlay-form/overlay-form.component.ts
+++ src/app/pages/overlay-form/overlay-form.component.ts
@@ -12,32 +12,36 @@ import { MatButtonModule } from '@angular/material/button';
 export class OverlayFormComponent {
   constructor(public dialogRef: MatDialogRef<OverlayFormComponent>) {}
 
-  displayedColumns = [
+  displayedFormaData = { nome: 'tp_forma', data: 'teste' };
+
+  displayedHeaderInfo = [
     { nome: 'nu_notificacao_atual', data: 'teste' },
     { nome: 'dt_notificacao_atual', data: 'teste' },
     { nome: 'no_nome_paciente', data: 'teste' },
-    { nome: 'st_baciloscopia_2_mes', data: 'teste' },
-    { nome: 'st_baciloscopia_4_mes', data: 'teste' },
-    { nome: 'st_baciloscopia_6_mes', data: 'teste' },
-    { nome: 'st_bacil_apos_6_mes', data: 'teste' },
-    { nome: 'nu_contato_examinado', data: 'teste' },
-    { nome: 'tp_antirretroviral_trat', data: 'teste' },
-    { nome: 'tp_molecular', data: 'teste' },
-    { nome: 'tp_situacao_encerramento', data: 'teste' },
-    { nome: 'dt_encerramento', data: 'teste' },
-    { nome: 'tp_cultura_escarro', data: 'teste' },
-    { nome: 'tp_sensibilidade', data: 'teste' },
-    { nome: 'tp_hiv', data: 'teste' },
-    { nome: 'tp_histopatologia', data: 'teste' },
-    { nome: 'ds_observacao', data: 'teste' },
-    { nome: 'st_baciloscopia_1_mes', data: 'teste' },
-    { nome: 'st_baciloscopia_3_mes', data: 'teste' },
-    { nome: 'st_baciloscopia_5_mes', data: 'teste' },
-    { nome: 'ds_estabelecimento', data: 'teste' },
-    { nome: 'tp_tratamento_acompanhamento', data: 'teste' },
-    { nome: 'tp_forma', data: 'teste' },
-    { nome: 'nu_contato', data: 'teste' },
-    { nome: 'co_municipio_transf', data: 'teste' }
+  ]
+
+  displayedGridColumns = [
+    { nome: 'Bac 1º Mês', data: 'teste' },
+    { nome: 'Bac 2º Mês', data: 'teste' },
+    { nome: 'Bac 3º Mês', data: 'teste' },
+    { nome: 'Bac 4º Mês', data: 'teste' },
+    { nome: 'Bac 5º Mês', data: 'teste' },
+    { nome: 'Bac 6º Mês', data: 'teste' },
+    { nome: 'Bac após 6º Mês', data: 'teste' },
+    { nome: 'Total contat. identif', data: 'teste' },
+    { nome: 'Total contat. exam.', data: 'teste' },
+    { nome: 'HIV', data: 'teste' },
+    { nome: 'TARV', data: 'teste' },
+    { nome: 'Cultura', data: 'teste' },
+    { nome: 'TRM-TB', data: 'teste' },
+    { nome: 'Histopat', data: 'teste' },
+    { nome: 'TS', data: 'teste' },
+    { nome: 'Realizado TDO?', data: 'teste' },
+    { nome: 'Situação Encerrada', data: 'teste' },
+    { nome: 'Se Transf', data: 'teste' },
+    { nome: 'Local de Transf (Mun/UF)', data: 'teste' },
+    { nome: 'Data de encerramento', data: 'teste' },
+    { nome: 'Data Últ. Comparecim.', data: 'teste' }
   ];
   
 
